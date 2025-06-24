6039a9db41b0ba8f12031c3257e2126213c35819
Mozammil <mozammiltarique007@gmail.com>
2025-06-24 17:25:04 +0530

    Add Home and Blog pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,10 @@
 import { createRoot } from "react-dom/client";
-import { BrowseRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./styles/index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowseRouter>
+  <BrowserRouter>
     <App />
-  </BrowseRouter>
+  </BrowserRouter>
 );
