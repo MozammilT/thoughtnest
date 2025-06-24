21cb4a6a7687c4433158cda9c2a31eafc160c343
Mozammil <mozammiltarique007@gmail.com>
2025-06-24 18:28:35 +0530

    Add Navbar component

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import { useNavigate } from "react-router-dom";
+
+function Navbar() {
+  const navigate = useNavigate();
+
+  return (
+    <nav className="flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32">
+      <img
+        onClick={() => navigate("/")}
+        className="w-50 h-15 cursor-pointer"
+        src="/ThoughtNest1.png"
+        alt="dummyLogoColored"
+      />
+      <button
+        onClick={() => navigate("/admin")}
+        className="group rounded-full bg-primary text-white flex items-center gap-2 px-6 py-2 cursor-pointer text-base"
+      >
+        Admin Login{" "}
+        <img
+          src="/arrow.svg"
+          className="group-hover:translate-x-3 transition-all duration-200"
+        />
+      </button>
+    </nav>
+  );
+}
+
+export default Navbar;
