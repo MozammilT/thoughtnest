17617d27514752fbd845be922cdb8836af16b0da
Mozammil <mozammiltarique007@gmail.com>
2025-06-30 00:36:17 +0530

    Add blog list component

--- client/src/pages/Admin/BloglistMenu.jsx
+++ client/src/pages/Admin/BloglistMenu.jsx
@@ -1,5 +1,56 @@
+import { useState, useEffect } from "react";
+import { blog_data } from "../../constants/assets.js";
+import TableItem from "@/components/admin/TableItem.jsx";
+
 function BloglistMenu() {
-  return <div>Blog List page</div>;
+  const [blog, setBlogs] = useState([]);
+
+  const fetchBlogs = async () => {
+    setBlogs(blog_data);
+  };
+
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
+
+  return (
+    <div>
+      <div className="flex m-4 mt-15 items-center gap-3 text-gray-600">
+        <img src="/dashboard_icon_4.svg" />
+        <p>All Blogs</p>
+      </div>
+
+      <div className="relative max-w-4xl overflow-x-auto shadow rounded-lg scrollbar-hide bg-white">
+        <table className="w-full text-sm text-gray-500">
+          <thead className="text-xs text-gray-600 text-left uppercase">
+            <tr>
+              <th scope="col" className="px-2 py-4 xl:px-6">
+                #
+              </th>
+              <th scope="col" className="px-2 py-4">
+                blog title
+              </th>
+              <th scope="col" className="px-2 py-4 max-sm:hidden">
+                date
+              </th>
+              <th scope="col" className="px-2 py-4 max-sm:hidden">
+                status
+              </th>
+              <th scope="col" className="px-2 py-4">
+                actions
+              </th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {blog.map((blog, idx) => (
+              <TableItem blog={blog} index={idx + 1} fetchBlogs={fetchBlogs} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default BloglistMenu;
