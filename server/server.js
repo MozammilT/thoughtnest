a707754761c522d320442db6adfa74fb6473e3ee
Mozammil <mozammiltarique007@gmail.com>
2025-07-02 15:46:10 +0530

    Refractor to add routes for admin

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import adminRoute from "./routes/adminRoute.js";
 
 connectDB();
 
@@ -16,6 +17,8 @@ app.use(express.json());
 app.get("/", (_, res) => {
   res.send("Api is running");
 });
+
+app.use("/api/admin", adminRoute);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
