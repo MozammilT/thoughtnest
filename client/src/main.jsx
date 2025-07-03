7913f58c5f06ddac43bf22cbad0ec61f264f9c43
Mozammil <mozammiltarique007@gmail.com>
2025-07-03 15:38:53 +0530

    Wrap App component with AppProvider for context integration

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,12 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./styles/index.css";
 import App from "./App.jsx";
+import { AppProvider } from "./context/AppContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <AppProvider>
+      <App />
+    </AppProvider>
   </BrowserRouter>
 );
