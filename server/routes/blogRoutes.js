95b42a386f456d8e90d960e4b5d09f0fcdbb87ee
Mozammil <mozammiltarique007@gmail.com>
2025-07-04 02:43:54 +0530

    Bug Fix

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import upload from "../middleware/uploadMiddleware.js";
 import { addBlog } from "../controllers/blogController.js";
 
-const blogRoute = express();
+const blogRoute = express.Router();
 
 blogRoute.post("/add", upload.single("image"), addBlog);
 
