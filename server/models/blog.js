d8ec7585d6095adaefa5fe35959f9adb241fc9cf
Mozammil <mozammiltarique007@gmail.com>
2025-07-04 01:02:05 +0530

    Add blog model schema for MongoDB integration

---
+++ server/models/blog.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const blogSchema = mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    category: { type: String, required: true },
+    image: { type: String, required: true },
+    isPublished: { type: Boolean, required: true },
+    subTitle: { type: String, required: true },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Blog = mongoose.model("Blog", blogSchema);
+export default Blog;
