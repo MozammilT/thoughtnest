721f55a1cac9185f1a61a16b7c80e97208c454b2
Mozammil <mozammiltarique007@gmail.com>
2025-07-05 16:05:01 +0530

    Add user reference to comments schema

--- server/models/comment.js
+++ server/models/comment.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const commentsSchema = mongoose.Schema(
   {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     blog: { type: mongoose.Schema.Types.ObjectId, ref: "Blog", required: true },
     name: { type: String, required: true },
     content: { type: String, required: true },
