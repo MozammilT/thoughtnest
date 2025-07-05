5e89973b4d7b0d2377a28a1f8c453eca3a5f271a
Mozammil <mozammiltarique007@gmail.com>
2025-07-05 19:23:34 +0530

    Refractor to conditionally navigate to dashboard or login based on admin status

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import { useNavigate } from "react-router-dom";
+import { useAppContext } from "../context/AppContext.jsx";
 
 function Navbar() {
   const navigate = useNavigate();
+  const { admin } = useAppContext();
 
   return (
     <nav className="flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32">
@@ -12,10 +14,10 @@ function Navbar() {
         alt="Thoughtnest-logo"
       />
       <button
-        onClick={() => navigate("/login")}
+        onClick={() => (admin ? navigate("/dashboard") : navigate("/login"))}
         className="group rounded-full bg-primary text-white flex items-center gap-2 px-6 py-2 cursor-pointer text-base"
       >
-        Admin Login{" "}
+        {admin ? "Dashboard" : "Admin Login"}
         <img
           src="/arrow.svg"
           className="group-hover:translate-x-3 transition-all duration-200"
