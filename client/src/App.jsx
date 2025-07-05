7c4621bf11d7fcdbd84b5dbacbf8868ca3e9bb46
Mozammil <mozammiltarique007@gmail.com>
2025-07-05 20:30:01 +0530

    Refactor App component to include Toaster for notifications

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,22 +9,24 @@ import CommentsMenu from "./pages/Admin/CommentsMenu";
 import Login from "./components/admin/Login";
 import SignUp from "./components/admin/SignUp";
 import "quill/dist/quill.snow.css";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blog/:id" element={<Blog />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/dashboard" element={<SidebarDemo />}>
-          <Route index element={<DashboardMenu />} />
-          <Route path="addBlog" element={<AddblogMenu />} />
-          <Route path="blogList" element={<BloglistMenu />} />
-          <Route path="comments" element={<CommentsMenu />} />
-        </Route>
-      </Routes>
+      <Toaster />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog/:id" element={<Blog />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/dashboard" element={<SidebarDemo />}>
+            <Route index element={<DashboardMenu />} />
+            <Route path="addBlog" element={<AddblogMenu />} />
+            <Route path="blogList" element={<BloglistMenu />} />
+            <Route path="comments" element={<CommentsMenu />} />
+          </Route>
+        </Routes>
     </div>
   );
 }
