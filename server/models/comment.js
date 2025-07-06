871f522ca1c22313da15cfae7bb87cf470b6ed71
Mozammil <mozammiltarique007@gmail.com>
2025-07-06 23:43:22 +0530

    Update: make user field optional and default isApproved to true

--- server/models/comment.js
+++ server/models/comment.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const commentsSchema = mongoose.Schema(
   {
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     blog: { type: mongoose.Schema.Types.ObjectId, ref: "Blog", required: true },
     name: { type: String, required: true },
     content: { type: String, required: true },
-    isApproved: { type: Boolean, default: false },
+    isApproved: { type: Boolean, default: true },
   },
   { timestamps: true }
 );
