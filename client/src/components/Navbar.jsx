7589f59ae81691b9cb363e508e977390b9b6ea84
Mozammil <mozammiltarique007@gmail.com>
2025-07-11 23:13:54 +0530

    Add dark theme and toggle button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,28 +1,40 @@
 import { useNavigate } from "react-router-dom";
 import { useAppContext } from "../context/AppContext.jsx";
+import { useTheme } from "../context/ThemeContext.jsx";
 
 function Navbar() {
   const navigate = useNavigate();
   const { admin } = useAppContext();
+  const { theme, toggleTheme } = useTheme();
+  const darkMode = theme === "dark";
 
   return (
-    <nav className="flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32">
+    <nav className="flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32 bg-white dark:bg-[#030712]/1">
       <img
         onClick={() => navigate("/")}
         className="w-45 h-15 cursor-pointer"
-        src="/main.png"
+        src={darkMode ? "/logo_dark1.png" : "/main.png"}
         alt="Thoughtnest-logo"
       />
-      <button
-        onClick={() => (admin ? navigate("/dashboard") : navigate("/login"))}
-        className="group rounded-full bg-primary text-white flex items-center gap-2 px-6 py-2 cursor-pointer text-base"
-      >
-        {admin ? "Dashboard" : "Admin Login"}
-        <img
-          src="/arrow.svg"
-          className="group-hover:translate-x-3 transition-all duration-200"
-        />
-      </button>
+      <div className="flex items-center gap-4">
+        <button
+          onClick={toggleTheme}
+          className="text-sm px-4 py-2 rounded-full bg-gray-200 dark:bg-gray-700 text-black dark:text-white cursor-pointer"
+        >
+          {darkMode ? "☀️" : "🌙"}
+        </button>
+
+        <button
+          onClick={() => (admin ? navigate("/dashboard") : navigate("/login"))}
+          className="group rounded-full bg-primary text-white flex items-center gap-2 px-6 py-2 cursor-pointer text-base"
+        >
+          {admin ? "Dashboard" : "Admin Login"}
+          <img
+            src="/arrow.svg"
+            className="group-hover:translate-x-3 transition-all duration-200"
+          />
+        </button>
+      </div>
     </nav>
   );
 }
