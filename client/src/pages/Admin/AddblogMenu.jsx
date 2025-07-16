1d268212d74580f5899fc07fa27282d7454ed31b
Mozammil <mozammiltarique007@gmail.com>
2025-07-16 21:08:30 +0530

    Improve responsiveness

--- client/src/pages/Admin/AddblogMenu.jsx
+++ client/src/pages/Admin/AddblogMenu.jsx
@@ -139,105 +139,107 @@ export function AddblogMenu() {
   return (
     <form
       onSubmit={submitHabndler}
-      className={`flex-1 h-full overflow-scroll ${
+      className={`w-full h-full flex justify-center items-start ${
         darkMode
           ? "bg-[#171717] text-gray-300"
           : "bg-yellow-50/30 text-gray-600"
       }`}
     >
       <div
-        className={`w-full max-w-3xl p-4 md:p-10 sm:m-10 shadow rounded ${
+        className={`w-full max-w-5xl h-[85vh] shadow rounded mt-5 ${
           darkMode ? "bg-[#414141]" : "bg-white"
-        }`}
+        } flex flex-col overflow-hidden`}
       >
-        <p>Upload Thumbnail</p>
-        <label htmlFor="image">
-          <img
-            src={image ? URL.createObjectURL(image) : "/upload_area.svg"}
-            alt="uplaod-area"
-            className="mt-2 h-16 rounded cursor-pointer"
-          />
-          <input
-            type="file"
-            id="image"
-            onChange={(e) => setImage(e.target.files[0])}
-            hidden
-            required
-          />
-        </label>
-
-        <p className="mt-4">Blog Title</p>
-        <input
-          required
-          value={title}
-          type="text"
-          placeholder="Enter the title"
-          onChange={(e) => setTitle(e.target.value)}
-          className={`w-full max-w-lg mt-2 p-2 border outline-none rounded ${
-            darkMode ? "border-gray-500 text-gray-100" : "border-gray-300"
-          }`}
-        />
-        <p className="mt-4">Sub Title</p>
-        <input
-          required
-          value={subTitle}
-          type="text"
-          placeholder="Enter the sub title"
-          onChange={(e) => setSubTitle(e.target.value)}
-          className={`w-full max-w-lg mt-2 p-2 border outline-none rounded ${
-            darkMode ? "border-gray-500 text-gray-100" : "border-gray-300"
-          }`}
-        />
-        <p className="mt-4">Blog Description</p>
-        <div className="max-w-lg pb-16 sm:pb-10 pt-2 relative">
-          <div className="relative">
-            <div
-              ref={editorRef}
-              className={`min-h-[200px] max-h-[500px] overflow-y-auto rounded border ${
-                darkMode ? "border-gray-500" : "border-gray-300"
-              }`}
+        <div className="flex-1 overflow-y-auto p-6 space-y-4">
+          <p>Upload Thumbnail</p>
+          <label htmlFor="image">
+            <img
+              src={image ? URL.createObjectURL(image) : "/upload_area.svg"}
+              alt="uplaod-area"
+              className="mt-2 h-16 rounded cursor-pointer"
             />
-            {loading && (
-              <div className="absolute top-[42px] bottom-0 left-0 right-0 flex items-center justify-center bg-white/30 z-10">
-                <div className="w-8 h-8 rounded-full border-2 border-t-white animate-spin" />
-              </div>
-            )}
+            <input
+              type="file"
+              id="image"
+              onChange={(e) => setImage(e.target.files[0])}
+              hidden
+              required
+            />
+          </label>
+
+          <p className="mt-4">Blog Title</p>
+          <input
+            required
+            value={title}
+            type="text"
+            placeholder="Enter the title"
+            onChange={(e) => setTitle(e.target.value)}
+            className={`w-full max-w-lg mt-2 p-2 border outline-none rounded ${
+              darkMode ? "border-gray-500 text-gray-100" : "border-gray-300"
+            }`}
+          />
+          <p className="mt-4">Sub Title</p>
+          <input
+            required
+            value={subTitle}
+            type="text"
+            placeholder="Enter the sub title"
+            onChange={(e) => setSubTitle(e.target.value)}
+            className={`w-full max-w-lg mt-2 p-2 border outline-none rounded ${
+              darkMode ? "border-gray-500 text-gray-100" : "border-gray-300"
+            }`}
+          />
+          <p className="mt-4">Blog Description</p>
+          <div className="max-w-lg pb-16 sm:pb-10 pt-2 relative">
+            <div className="relative">
+              <div
+                ref={editorRef}
+                className={`max-sm:h-[200px] min-h-[200px] md:max-h-[600px] overflow-y-auto rounded border ${
+                  darkMode ? "border-gray-500" : "border-gray-300"
+                }`}
+              />
+              {loading && (
+                <div className="absolute top-[42px] bottom-0 left-0 right-0 flex items-center justify-center bg-white/30 z-10">
+                  <div className="w-8 h-8 rounded-full border-2 border-t-white animate-spin" />
+                </div>
+              )}
+            </div>
+            <button
+              type="button"
+              disabled={loading}
+              onClick={generateContentHandler}
+              className="absolute bottom-1 right-2 ml-2 text-sm text-white bg-black/70 py-1.5 px-4 rounded cursor-pointer hover:underline"
+            >
+              {loading ? "Generating" : "Generate with AI"}
+            </button>
+          </div>
+          <p className="mt-4 mb-4">Blog Category</p>
+          <SelectDemo category={category} setCategory={setCategory} />
+
+          <div className="flex gap-4 mt-5">
+            <p>Publish Now</p>
+            <input
+              type="checkbox"
+              checked={isPublished}
+              className="hover:scale-125 cursor-pointer transition-all duration-200"
+              onChange={(e) => setIsPublished(e.target.checked)}
+            />
           </div>
           <button
-            type="button"
             disabled={loading}
-            onClick={generateContentHandler}
-            className="absolute bottom-1 right-2 ml-2 text-sm text-white bg-black/70 py-1.5 px-4 rounded cursor-pointer hover:underline"
+            type="submit"
+            className="bg-primary px-4 py-1.5 mt-5 rounded min-w-[120px] text-white cursor-pointer hover:scale-105 transition-all duration-200 max-sm:mb-10"
           >
-            {loading ? "Generating" : "Generate with AI"}
+            {buttonLoading ? (
+              <div className="flex items-center gap-3 justify-center">
+                <div className="w-5 h-5 rounded-full border-2 border-t-primary animate-spin" />
+                <p>Adding</p>
+              </div>
+            ) : (
+              "Add Blog"
+            )}
           </button>
         </div>
-        <p className="mt-4 mb-4">Blog Category</p>
-        <SelectDemo category={category} setCategory={setCategory} />
-
-        <div className="flex gap-4 mt-5">
-          <p>Publish Now</p>
-          <input
-            type="checkbox"
-            checked={isPublished}
-            className="hover:scale-125 cursor-pointer transition-all duration-200"
-            onChange={(e) => setIsPublished(e.target.checked)}
-          />
-        </div>
-        <button
-          disabled={loading}
-          type="submit"
-          className="bg-primary px-4 py-1.5 mt-5 rounded min-w-[120px] text-white cursor-pointer hover:scale-105 transition-all duration-200"
-        >
-          {buttonLoading ? (
-            <div className="flex items-center gap-3 justify-center">
-              <div className="w-5 h-5 rounded-full border-2 border-t-primary animate-spin" />
-              <p>Adding</p>
-            </div>
-          ) : (
-            "Add Blog"
-          )}
-        </button>
       </div>
     </form>
   );
