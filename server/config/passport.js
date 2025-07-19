65e9de13aa8a7e61e227938acd71bbb0721a9ab6
Mozammil <mozammiltarique007@gmail.com>
2025-07-20 00:57:47 +0530

    Add Google O'Auth strategy

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,5 +1,6 @@
 import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
 
@@ -7,7 +8,7 @@ import bcrypt from "bcrypt";
 passport.use(
   new LocalStrategy(
     {
-      usernameField: "identifier", // matches your frontend field
+      usernameField: "identifier",
       passwordField: "password",
     },
     async (identifier, password, done) => {
@@ -22,6 +23,50 @@ passport.use(
         if (!match) return done(null, false, { message: "Incorrect password" });
 
         return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    }
+  )
+);
+
+//Google O'Auth Strategy
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/api/admin/auth/google/dashboard",
+    },
+    async (asseccToken, refreshToken, profile, done) => {
+      try {
+        console.log("Google O'Auth profile: ", profile);
+        try {
+          const firstName = profile.name.givenName;
+          const lastName = profile.name.familyName;
+          const username = (
+            firstName + (lastName ? lastName : "")
+          ).toLowerCase();
+          console.log(username, profile.emails[0].value, profile.id);
+
+          const user = await User.findOne({
+            email: profile.emails[0].value,
+          });
+          if (!user) {
+            const newUser = await User.create({
+              username,
+              email: profile.emails[0].value,
+              password: profile.id * process.env.SALT_ROUNDS,
+              image: profile.photos[0].value,
+            });
+            return done(null, newUser);
+          } else {
+            return done(null, user);
+          }
+        } catch (err) {
+          console.log("Google auth error: ", err);
+          return done(err);
+        }
       } catch (err) {
         return done(err);
       }
