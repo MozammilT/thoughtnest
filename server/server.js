006190e6b763b9bdeedae929e9a92b284abda758
Mozammil <mozammiltarique007@gmail.com>
2025-07-21 00:45:35 +0530

    Add backend live URL to CORS

--- server/server.js
+++ server/server.js
@@ -17,7 +17,11 @@ const port = 3000;
 //Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://thoughtnest-backend.vercel.app",
+    ],
     credentials: true,
   })
 );
